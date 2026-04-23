aa03e1327adb916074239565f2630941126acab8
TheGrumpyGameDev <ernestpazera@gmail.com>
2026-04-23 05:13:14 -0500

    its yer basic repl that you can Quit.

---
+++ src/TGGD.ROTRBIF/Program.fs
@@ -0,0 +1,35 @@
+﻿open Spectre.Console
+open System
+
+type State = {
+        Alive: bool
+    }
+
+let rec repl (state:State option) : State option =
+    if state.IsSome then
+        let input : string = 
+            "[olive]>[/]"
+            |> AnsiConsole.Ask
+        match input with
+        | "Quit." -> None
+        | _ ->
+            "[red]INVALID INPUT![/]"
+            |> AnsiConsole.MarkupLine
+            state
+        |> repl
+    else
+        None
+
+Console.BackgroundColor <- ConsoleColor.Yellow
+AnsiConsole.Clear()
+
+let figlet = FigletText("ROTRBIF# of SPLORR!!")
+figlet.Color <- Color.Fuchsia
+
+figlet
+|> AnsiConsole.Write
+
+{ Alive = true }
+|> Some
+|> repl
+|> ignore
