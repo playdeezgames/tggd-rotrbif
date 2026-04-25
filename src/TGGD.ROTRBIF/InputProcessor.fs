919842c188394950906a07a7db9cb5ec547395eb
TheGrumpyGameDev <ernestpazera@gmail.com>
2026-04-25 05:15:28 -0500

    abvout to do some wizard stuf!

--- src/TGGD.ROTRBIF/InputProcessor.fs
+++ src/TGGD.ROTRBIF/InputProcessor.fs
@@ -2,6 +2,33 @@ namespace TGGD.ROTRBIF
 
 [<AutoOpen>]
 module internal InputProcessor =
+    let private processInvalidCommand (context: MetaphorContext) : MetaphorContext option =
+        context 
+        |> MetaphorContext.doSideEffect MetaphorContext.invalidCommand
+        |> Some
+    let private processCommand (command:Command) (context: MetaphorContext) : MetaphorContext option =
+        match command with
+        | Statement s -> 
+            match s with
+            | Quit -> 
+                None
+            | Turn turn ->
+                context
+                |> MetaphorContext.transformState (MetaphorState.executeTurn turn)
+                |> MetaphorContext.doSideEffect (MetaphorContext.reportTurn turn)
+                |> Some
+            | _ -> 
+                context |> processInvalidCommand
+        | Question q -> 
+            match q with
+            | Status ->
+                context 
+                |> MetaphorContext.doSideEffect MetaphorContext.showStatus
+                |> Some
+            | _ ->
+                context |> processInvalidCommand
+        | _ -> 
+            context |> processInvalidCommand
     type MetaphorContext with
         static member internal processInput 
                 (input: string) 
@@ -9,35 +36,8 @@ module internal InputProcessor =
                 : MetaphorContext option =
             match input |> Command.parse with
             | Some command -> 
-                match command with
-                | Statement s -> 
-                    match s with
-                    | Quit -> 
-                        None
-                    | Turn turn ->
-                        context
-                        |> MetaphorContext.transformState (MetaphorState.executeTurn turn)
-                        |> MetaphorContext.doSideEffect (MetaphorContext.reportTurn turn)
-                        |> Some
-                    | _ -> 
-                        context 
-                        |> MetaphorContext.doSideEffect MetaphorContext.invalidCommand
-                        |> Some
-                | Question q -> 
-                    match q with
-                    | Status ->
-                        context 
-                        |> MetaphorContext.doSideEffect MetaphorContext.showStatus
-                        |> Some
-                    | _ ->
-                        context
-                        |> MetaphorContext.doSideEffect MetaphorContext.invalidCommand
-                        |> Some
-                | _ -> 
-                    context 
-                    |> MetaphorContext.doSideEffect MetaphorContext.invalidCommand
-                    |> Some
+                context
+                |> processCommand command
             | _ -> 
                 context 
-                |> MetaphorContext.doSideEffect MetaphorContext.invalidCommand
-                |> Some
+                |> processInvalidCommand
