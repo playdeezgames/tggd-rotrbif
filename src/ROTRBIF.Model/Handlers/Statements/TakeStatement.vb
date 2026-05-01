06dd8b6c5a60921086356579ea5a7a8596a474f1
TheGrumpyGameDev <ernestpazera@gmail.com>
2026-05-01 05:13:50 -0500

    you have to bend over to pick things up.

--- src/ROTRBIF.Model/Handlers/Statements/TakeStatement.vb
+++ src/ROTRBIF.Model/Handlers/Statements/TakeStatement.vb
@@ -6,6 +6,11 @@
             Return
         End If
         Dim avatar = context.World.Avatar
+        'You can't reach the ground from this height.
+        If Not avatar.GetTag(Tags.BENT_OVER) Then
+            context.Output($"{avatar.GetName} cannot reach the ground from this height.")
+            Return
+        End If
         Dim item = avatar.Location.Inventory.TryFindItemByName(itemName)
         If item Is Nothing Then
             HandleInvalidCommand(context)
