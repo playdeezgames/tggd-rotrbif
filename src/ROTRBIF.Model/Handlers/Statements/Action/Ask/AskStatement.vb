f434aaa70d9a95e357915ea654e0645695104a34
TheGrumpyGameDev <ernestpazera@gmail.com>
2026-05-11 05:45:38 -0500

    healer heals

--- src/ROTRBIF.Model/Handlers/Statements/Action/Ask/AskStatement.vb
+++ src/ROTRBIF.Model/Handlers/Statements/Action/Ask/AskStatement.vb
@@ -3,22 +3,47 @@
 Friend Module AskStatement
     Const AboutToken As String = "about"
     Const WorkTopic As String = "work"
-    Private ReadOnly topicTable As IReadOnlyDictionary(Of String, Action(Of IModelContext, ICharacter, ICharacter)) =
-        New Dictionary(Of String, Action(Of IModelContext, ICharacter, ICharacter)) From
+    Const HealingTopic As String = "healing"
+    Private ReadOnly topicObjectTable As IReadOnlyDictionary(Of String, IReadOnlyDictionary(Of ObjectIdentifier, Action(Of IModelContext, ICharacter, ICharacter))) =
+        New Dictionary(Of String, IReadOnlyDictionary(Of ObjectIdentifier, Action(Of IModelContext, ICharacter, ICharacter))) From
         {
-            {WorkTopic, AddressOf AskAboutWorkTopic.Handle}
+            {
+                WorkTopic,
+                New Dictionary(Of ObjectIdentifier, Action(Of IModelContext, ICharacter, ICharacter)) From
+                {
+                    {ObjectIdentifier.GORACHAN, AddressOf CellarQuest.AskGorachanAboutWork}
+                }
+            },
+            {
+                HealingTopic,
+                New Dictionary(Of ObjectIdentifier, Action(Of IModelContext, ICharacter, ICharacter)) From
+                {
+                    {ObjectIdentifier.HEALER, AddressOf AskHealerAboutHealing}
+                }
+            }
         }
+
+    Private Sub AskHealerAboutHealing(
+                                     context As IModelContext,
+                                     interrogator As ICharacter,
+                                     deponent As ICharacter)
+        context.Output($"{deponent.GetName} says:")
+        context.Output($"""Nothing matters.""")
+        context.Output($"{deponent.GetName} fully heals {interrogator.GetName}.")
+        interrogator.SetStatistic(Statistics.HEALTH, interrogator.GetStatisticMaximum(Statistics.HEALTH))
+    End Sub
+
     Friend Sub Handle(context As IModelContext)
         Dim avatar = context.World.Avatar
         If avatar.IsDead Then
             HandleInvalidCommand(context)
             Return
         End If
-        If Not context.HasTokens Then
+        If Not context.HasToken(AboutToken) Then
             HandleInvalidCommand(context)
             Return
         End If
-        Dim otherName = context.ReadToken
+        Dim otherName = context.ReadUntilToken(AboutToken)
         Dim other = avatar.Location.FindOtherByName(avatar, otherName)
         If other Is Nothing Then
             HandleInvalidCommand(context)
@@ -28,20 +53,27 @@ Friend Module AskStatement
             HandleInvalidCommand(context)
             Return
         End If
-        If context.ReadToken <> AboutToken Then
-            HandleInvalidCommand(context)
-            Return
-        End If
+        context.DiscardToken()
         Dim topic As String = context.ReadRemainingTokens
         If topic Is Nothing Then
             HandleInvalidCommand(context)
             Return
         End If
-        Dim handler As Action(Of IModelContext, ICharacter, ICharacter) = Nothing
-        If Not topicTable.TryGetValue(topic, handler) Then
-            HandleInvalidCommand(context)
+        If Not topicObjectTable.ContainsKey(topic) Then
+            context.Output($"{other.GetName} has nothing to say to {avatar.GetName} about {topic}.")
             Return
         End If
-        handler.Invoke(context, avatar, other)
+        context.Output($"{avatar.GetName} asks {other.GetName} about {topic}.")
+        Dim objectIdentifier = other.GetObjectIdentifier()
+        If Not objectIdentifier.HasValue Then
+            context.Output($"{other.GetName} has nothing to say to {avatar.GetName} about {topic}.")
+            Return
+        End If
+        Dim handler As Action(Of IModelContext, ICharacter, ICharacter) = Nothing
+        If Not topicObjectTable(topic).TryGetValue(objectIdentifier.Value, handler) Then
+            context.Output($"{other.GetName} has nothing to say to {avatar.GetName} about {topic}.")
+            Return
+        End If
+        handler(context, avatar, other)
     End Sub
 End Module
